f243d8d6eee07438f00eca24fb870e626aa87116
hcz <hczhcz@users.noreply.github.com>
2017-09-20 02:29:28 -0400

    Update ecna16.waif.cpp

--- kattis/ecna16.waif.cpp
+++ kattis/ecna16.waif.cpp
@@ -1,5 +1,4 @@
 // https://ecna16.kattis.com/problems/ecna16.waif
-// not AC yet
 
 #include <cstdlib>
 #include <cstdint>
@@ -7,52 +6,73 @@
 #include <cmath>
 #include <cstring>
 #include <string>
+#include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
 int n, m, p;
+bool toy_in_cat[1000];
 
-int child_sum[128];
-int child_want[128][128];
-int toy_cat[128];
+struct Edge {
+    int from, to, cap, flow;
+};
 
-int child_order[128];
+struct EdmondsKarp {
+    vector<Edge> edges;
+    vector<int> outs[1000];
 
-bool used[128];
-int cat_limit[128];
-int filled;
-int best[128];
+    void add(int from, int to, int cap){
+        edges.push_back(Edge {from, to, cap, 0});
+        outs[from].push_back(edges.size() - 1);
+        edges.push_back(Edge {to, from, 0, 0});
+        outs[to].push_back(edges.size() - 1);
+    }
 
-double complexity;
-double c;
+    int solve(int from, int to){
+        int flow = 0;
 
-void search(int d) {
-    if (best[d] < filled) best[d] = filled;
+        while (true) {
+            int amount[1000];
+            int route[1000];
+            queue<int> q;
 
-    if (d <= n && filled + n - d + 1 > best[n + 1] && filled > best[d] - 3) {
-        int child = child_order[d];
+            memset(amount, 0, sizeof(amount));
 
-        if (rand() < c * RAND_MAX)
-        for (int i = child_sum[child]; i >= 1; --i) {
-            int toy = child_want[child][i];
-            int cat = toy_cat[toy];
+            q.push(from);
 
-            if (used[toy]) continue;
-            if (!cat_limit[cat]) continue;
+            amount[from] = 1 << 30;
+            while (!q.empty() && !amount[to]){
+                int i = q.front();
+                q.pop();
 
-            used[toy] = true;
-            cat_limit[cat] -= 1;
-            filled += 1;
-            search(d + 1);
-            filled -= 1;
-            cat_limit[cat] += 1;
-            used[toy] = false;
+                for (int j = 0; j < outs[i].size(); ++j){
+                    Edge &e = edges[outs[i][j]];
+
+                    if (!amount[e.to] && e.cap > e.flow){
+                        amount[e.to] = min(amount[i], e.cap - e.flow);
+                        route[e.to] = outs[i][j];
+
+                        q.push(e.to);
+                    }
+                }
+            }
+            if (!amount[to]) break;
+
+            for (int i = to; i != from; i = edges[route[i]].from){
+                edges[route[i]].flow += amount[i];
+                edges[route[i] ^ 1].flow -= amount[i];
+            }
+
+            flow += amount[to];
         }
 
-        search(d + 1);
+        return flow;
     }
-}
+};
+
+EdmondsKarp ek;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -60,42 +80,47 @@ int main() {
 
     cin >> n >> m >> p;
 
-    for (int child = 1; child <= n; ++child) {
-        cin >> child_sum[child];
-        for (int i = 1; i <= child_sum[child]; ++i) {
-            cin >> child_want[child][i];
-        }
-        complexity += log(child_sum[child] + 1);
-    }
-    for (int cat = 1; cat <= p; ++cat) {
-        int cat_tot;
-        cin >> cat_tot;
-        for (int i = 1; i <= cat_tot; ++i) {
+    for (int child = 1; child <= n; ++child){
+        ek.add(0, child, 1);
+
+        int toy_tot;
+        cin >> toy_tot;
+
+        for(int i = 1; i <= toy_tot; ++i){
             int toy;
             cin >> toy;
-            toy_cat[toy] = cat;
-        }
-        cin >> cat_limit[cat];
-    }
-    cat_limit[0] = 128;
 
-    for (int i = 1; i <= n; ++i) {
-        child_order[i] = i;
-    }
-    for (int i = 1; i <= n; ++i) {
-        for (int j = i + 1; j <= n; ++j) {
-            if (child_sum[child_order[i]] > child_sum[child_order[j]]) {
-                int tmp = child_order[i];
-                child_order[i] = child_order[j];
-                child_order[j] = tmp;
-            }
+            ek.add(child, n + toy, 1);
         }
     }
 
-    c = exp((18 - complexity) / n);
-    search(1);
+    for (int cat = 1; cat <= p; ++cat){
+        int cat_tot;
+        cin >> cat_tot;
 
-    cout << best[n + 1] << endl;
+        for (int i = 1; i <= cat_tot; ++i){
+            int toy;
+            cin >> toy;
+
+            ek.add(n + toy, n + m + cat, 1);
+            toy_in_cat[toy] = true;
+        }
+
+        int cat_limit;
+        cin >> cat_limit;
+
+        ek.add(n + m + cat, 999, cat_limit);
+    }
+
+    for (int toy = 1; toy <= m; ++toy) {
+        if (!toy_in_cat[toy]) {
+            ek.add(n + toy, n + m + p + 1, 1);
+        }
+    }
+
+    ek.add(n + m + p + 1, 999, 1000);
+
+    cout << ek.solve(0, 999) << endl;
 
     return 0;
 }
