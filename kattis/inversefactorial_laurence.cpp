eff494e073259b92948dd830c0af9bf26ea95d29
Laurence <liuxy6@gmail.com>
2017-09-21 18:03:37 -0400

    Add a solution to inversefactorial

---
+++ kattis/inversefactorial_laurence.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+
+	unsigned long long pref = stoull(s.substr(0, 10));
+
+	double prefix = 1;
+	for (int i = 1; i < 205025; i++) {
+		prefix *= i;
+		while (prefix > 1e10)
+			prefix /= 10;
+		if ((unsigned long long)floor(prefix) == pref)
+			cout << i << endl;
+	}
+}
