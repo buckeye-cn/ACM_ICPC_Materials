cd442a5e914b7fbcd552b3f1a92d98cbc07f1e54
Laurence <liuxy6@gmail.com>
2017-09-23 23:47:30 -0400

    Cleanup redrover

--- kattis/ecna16.redrover_laurence.cpp
+++ kattis/ecna16.redrover_laurence.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <string>
@@ -6,10 +7,11 @@
 
 using namespace std;
 
-bool EQ[100][100] = {};
+string s;
 int LEN;
+bool EQ[100][100] = {};
 
-void output_eq(string& s) {
+void output_eq() {
 #ifdef DEBUG
 	cerr << "  ";
 	for (char c : s) {
@@ -18,7 +20,7 @@ void output_eq(string& s) {
 	cerr << endl;
 	for (int i = 0; i < LEN; i++) {
 		cerr << s[i] << " ";
-		for (int j = 0; j <= i; j++) {
+		for (int j = 0; j < LEN; j++) {
 			cerr << EQ[i][j] << " ";
 		}
 		cerr << endl;
@@ -27,45 +29,39 @@ void output_eq(string& s) {
 }
 
 int main() {
-	string s;
 	cin >> s;
 
 	LEN = s.length();
 
 	for (int i = 0; i < LEN; i++)
-		for (int j = 0; j <= i; j++)
+		for (int j = 0; j < LEN; j++)
 			EQ[i][j] = s[i] == s[j];
 
-	output_eq(s);
+	output_eq();
 
-	int min = LEN;
+	int min_len = LEN;
 	for (int len = 2; len < LEN/2; len++) {
 		for (int i = 1; i < LEN-len+1; i++) {
-			for (int j = 0; j <= i; j++) {
-				EQ[i][j] = EQ[i][j] && (s[i+len-1] == s[j+len-1]);
+			for (int j = 0; j < LEN-len+1; j++) {
+				EQ[i][j] &= s[i+len-1] == s[j+len-1];
 			}
 		}
-		output_eq(s);
+		output_eq();
 
 		map<string, int> c = {};
-		map<string, int> last = {};
 		for (int i = 1; i < LEN-len+1; i++) {
-			for (int j = 0; j <= i; j++) {
+			string subs = s.substr(i, len);
+			if (c.count(subs)) {
+				continue;
+			}
+			c[subs] = 0;
+			int j = 0;
+			while (j < LEN-len+1) {
 				if (EQ[i][j]) {
-					string subs = s.substr(i, len);
-
-					int _last = -len - 1;
-					if (last.find(subs) != last.end()) {
-						_last = last[subs];
-					}
-					if (_last <= j - len) {
-						if (c.find(subs) == c.end()) {
-							c[subs] = 1;
-						} else {
-							c[subs] += 1;
-						}
-						last[subs] = j;
-					}
+					c[subs] += 1;
+					j += len;
+				} else {
+					j += 1;
 				}
 			}
 		}
@@ -74,20 +70,19 @@ int main() {
 
 		int max_m = 0;
 		for (auto p : c) {
-			max_m = p.second > max_m ? p.second : max_m;
+			max_m = max(max_m, p.second);
 #ifdef DEBUG
 			cerr << p.first << "\t" << p.second << endl;
 #endif
 		}
 
 		int len_res = LEN - max_m * (len-1) + len;
-		min = len_res < min ? len_res : min;
+		min_len = min(min_len, len_res);
 
 #ifdef DEBUG
-		cerr << max_m << " " << len_res << endl;
-		cerr << "-----" << endl;
+		cerr << max_m << " " << len_res << endl << "---------" << endl;
 #endif
 	}
 
-	cout << min << endl;
+	cout << min_len << endl;
 }
