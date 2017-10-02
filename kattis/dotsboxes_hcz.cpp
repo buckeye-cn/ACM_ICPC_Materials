7b261b5b95ea44bf8faa4f23793046950ad39fe2
hcz <hczhcz@users.noreply.github.com>
2017-10-02 17:50:13 -0400

    Create dotsboxes_hcz.cpp

---
+++ kattis/dotsboxes_hcz.cpp
@@ -0,0 +1,158 @@
+// https://open.kattis.com/problems/dotsboxes
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int n;
+bool ud[100][100];
+bool lr[100][100];
+int slot[100][100];
+
+template <int N>
+struct EdmondsKarp {
+    struct Edge {
+        int from, to, cap, flow;
+    };
+
+    vector<Edge> edges;
+    vector<int> outs[N];
+
+    void add(int from, int to, int cap){
+        if (cap > 0) {
+            edges.push_back(Edge {from, to, cap, 0});
+            outs[from].push_back(edges.size() - 1);
+            edges.push_back(Edge {to, from, 0, 0});
+            outs[to].push_back(edges.size() - 1);
+        }
+    }
+
+    int solve(int from, int to){
+        int flow = 0;
+
+        while (true) {
+            int amount[N];
+            int route[N];
+            queue<int> q;
+
+            memset(amount, 0, sizeof(amount));
+
+            q.push(from);
+
+            amount[from] = 1 << 30;
+            while (!q.empty() && !amount[to]){
+                int i = q.front();
+                q.pop();
+
+                for (int j = 0; j < outs[i].size(); ++j){
+                    Edge &e = edges[outs[i][j]];
+
+                    if (!amount[e.to] && e.cap > e.flow){
+                        amount[e.to] = min(amount[i], e.cap - e.flow);
+                        route[e.to] = outs[i][j];
+
+                        q.push(e.to);
+                    }
+                }
+            }
+            if (!amount[to]) break;
+
+            for (int i = to; i != from; i = edges[route[i]].from){
+                edges[route[i]].flow += amount[to];
+                edges[route[i] ^ 1].flow -= amount[to];
+            }
+
+            flow += amount[to];
+        }
+
+        return flow;
+    }
+};
+
+EdmondsKarp<10000> ek;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        char c;
+
+        for (int j = 1; j <= n; ++j) {
+            cin >> c;
+
+            if (j == n) break;
+
+            cin >> c;
+            ud[i][j] = c == '-';
+        }
+
+        if (i == n) break;
+
+        for (int j = 1; j <= n; ++j) {
+            cin >> c;
+            lr[i][j] = c == '|';
+
+            if (j == n) break;
+
+            cin >> c;
+        }
+    }
+
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            slot[i][j] = !ud[i][j] + !ud[i + 1][j] + !lr[i][j] + !lr[i][j + 1] - 1;
+
+            if ((i ^ j) & 1) {
+                ek.add(100 * i + j, 9999, slot[i][j]);
+            } else {
+                ek.add(9998, 100 * i + j, slot[i][j]);
+            }
+
+            // cout << slot[i][j] << ' ';
+        }
+
+        // cout << endl;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (j == n) break;
+
+            if (!ud[i][j]) {
+                if ((i ^ j) & 1) {
+                    ek.add(100 * (i - 1) + j, 100 * i + j, 1);
+                } else {
+                    ek.add(100 * i + j, 100 * (i - 1) + j, 1);
+                }
+            }
+        }
+
+        if (i == n) break;
+
+        for (int j = 1; j <= n; ++j) {
+            if (!lr[i][j]) {
+                if ((i ^ j) & 1) {
+                    ek.add(100 * i + (j - 1), 100 * i + j, 1);
+                } else {
+                    ek.add(100 * i + j, 100 * i + (j - 1), 1);
+                }
+            }
+        }
+    }
+
+    cout << ek.solve(9998, 9999) + 1 << endl;
+
+    return 0;
+}
