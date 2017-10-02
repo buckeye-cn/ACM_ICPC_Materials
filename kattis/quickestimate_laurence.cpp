f7f6006bf0e778bfbb7a546934158409bbff5bb0
Laurence <liuxy6@gmail.com>
2017-10-02 10:42:59 -0400

    Add a solution to quickestimate

---
+++ kattis/quickestimate_laurence.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	string n;
+	for (int i = 0; i < N; i++) {
+		cin >> n;
+		cout << n.length() << "\n";
+	}
+}
