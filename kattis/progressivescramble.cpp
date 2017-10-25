f0638f607f69ff4e31bdf0c5fd74dfe71f227aa7
hcz <hczhcz@users.noreply.github.com>
2017-10-25 16:20:30 -0400

    Create progressivescramble.cpp

---
+++ kattis/progressivescramble.cpp
@@ -0,0 +1,60 @@
+// https://open.kattis.com/problems/progressivescramble
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        char cmd;
+        cin >> cmd;
+
+        cin.get();
+        int last = 0;
+        while (true) {
+            char c = cin.get();
+            if (c == '\n') break;
+
+            int k = 0;
+            if (c >= 'a' && c <= 'z') {
+                k = c - 'a' + 1;
+            }
+
+            int result;
+            if (cmd == 'e') {
+                last = last + k;
+                last %= 27;
+                result = last;
+            } else {
+                last = k - last + 27;
+                last %= 27;
+                result = last;
+                last = k;
+            }
+            if (result == 0) {
+                cout << ' ';
+            } else {
+                cout << char('a' + result - 1);
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
