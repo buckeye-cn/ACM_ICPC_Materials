a47c1a4acbf35d592dd0bdee91094e4e4a8ea4a5
hcz <hczhcz@users.noreply.github.com>
2017-10-25 18:29:33 -0400

    Update greetingcard.cpp

--- kattis/naq17/greetingcard.cpp
+++ kattis/naq17/greetingcard.cpp
@@ -16,10 +16,9 @@ using namespace std;
 int n;
 long x[200000];
 long y[200000];
-vector<int> x2018[2018][999];
-vector<int> x1680[1680][1118];
-vector<int> y1680[1118][1680];
-vector<int> y2018[999][2018];
+vector<int> *x2018[1009][1009];
+vector<int> *x1680[560][559];
+vector<int> *x1118[559][560];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -35,27 +34,38 @@ int main() {
         x[i] = xx;
         y[i] = yy;
 
-        x2018[xx % 2018][yy % 999].push_back(i);
-        x1680[xx % 1680][yy % 1118].push_back(i);
-        y1680[xx % 1118][yy % 1680].push_back(i);
-        y2018[xx % 999][yy % 2018].push_back(i);
+        if (!x2018[xx % 1009][yy % 1009])
+            x2018[xx % 1009][yy % 1009] = new vector<int>();
+        x2018[xx % 1009][yy % 1009]->push_back(i);
+        if (!x1680[xx % 560][yy % 559])
+            x1680[xx % 560][yy % 559] = new vector<int>();
+        x1680[xx % 560][yy % 559]->push_back(i);
+        if (!x1118[xx % 559][yy % 560])
+            x1118[xx % 559][yy % 560] = new vector<int>();
+        x1118[xx % 559][yy % 560]->push_back(i);
     }
 
     long count = 0;
 
-    for (int p = 0; p < 2018; ++p)
-    for (int q = 0; q < 999; ++q)
-    for (int i: x2018[p][q])
-    for (int j: x2018[p][q]) {
+    for (int p = 0; p < 1009; ++p)
+    for (int q = 0; q < 1009; ++q)
+    if (x2018[p][q])
+    for (int i: *x2018[p][q])
+    for (int j: *x2018[p][q]) {
+        if (i == j) continue;
         if (x[i] + 2018 == x[j] && y[i] == y[j]) {
+            count += 1;
+        } else if (x[i] == x[j] && y[i] + 2018 == y[j]) {
             count += 1;
         }
     }
 
-    for (int p = 0; p < 1680; ++p)
-    for (int q = 0; q < 1118; ++q)
-    for (int i: x1680[p][q])
-    for (int j: x1680[p][q]) {
+    for (int p = 0; p < 560; ++p)
+    for (int q = 0; q < 559; ++q)
+    if (x1680[p][q])
+    for (int i: *x1680[p][q])
+    for (int j: *x1680[p][q]) {
+        if (i == j) continue;
         if (x[i] + 1680 == x[j] && y[i] + 1118 == y[j]) {
             count += 1;
         } else if (x[i] + 1680 == x[j] && y[i] - 1118 == y[j]) {
@@ -63,22 +73,15 @@ int main() {
         }
     }
 
-    for (int p = 0; p < 1118; ++p)
-    for (int q = 0; q < 1680; ++q)
-    for (int i: y1680[p][q])
-    for (int j: y1680[p][q]) {
+    for (int p = 0; p < 559; ++p)
+    for (int q = 0; q < 560; ++q)
+    if (x1118[p][q])
+    for (int i: *x1118[p][q])
+    for (int j: *x1118[p][q]) {
+        if (i == j) continue;
         if (x[i] + 1118 == x[j] && y[i] + 1680 == y[j]) {
             count += 1;
         } else if (x[i] + 1118 == x[j] && y[i] - 1680 == y[j]) {
-            count += 1;
-        }
-    }
-
-    for (int p = 0; p < 999; ++p)
-    for (int q = 0; q < 2018; ++q)
-    for (int i: y2018[p][q])
-    for (int j: y2018[p][q]) {
-        if (x[i] == x[j] && y[i] + 2018 == y[j]) {
             count += 1;
         }
     }
