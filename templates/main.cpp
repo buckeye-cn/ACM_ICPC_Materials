554e0a9e36d613465fa4b98ad5a708abeef94207
hcz <hczhcz@users.noreply.github.com>
2017-10-27 00:00:28 -0400

    Create main.cpp

---
+++ templates/main.cpp
@@ -0,0 +1,40 @@
+// code template for competitions
+// g++ --std=c++11 -Wall -g -O0 ???.cpp && time ./a.out
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <algorithm>
+#include <tuple>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <random>
+#include <iostream>
+
+using namespace std;
+
+long n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    cin >> n;
+
+    for (long i = 0; i < n; ++i) {
+        long x, y;
+        cin >> x >> y;
+    }
+
+    cout << "Hello\n";
+
+    return 0;
+}
