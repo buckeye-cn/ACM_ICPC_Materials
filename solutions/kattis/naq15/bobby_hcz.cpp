6620678fa376cfd16baf7da5c5344eaa57d17d45
hcz <hczhcz@users.noreply.github.com>
2017-11-04 18:03:58 -0400

    Update bobby_hcz.cpp

--- solutions/kattis/naq15/bobby_hcz.cpp
+++ solutions/kattis/naq15/bobby_hcz.cpp
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-long pas[12][12];
+long pas[11][11];
+long pwr[21][11];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -18,7 +19,7 @@ int main() {
     cout.precision(10);
 
     pas[0][0] = 1;
-    for (int i = 1; i < 12; ++i) {
+    for (int i = 1; i <= 10; ++i) {
         pas[i][0] = 1;
         for (int j = 1; j < i; ++j) {
             pas[i][j] = pas[i - 1][j - 1] + pas[i - 1][j];
@@ -26,30 +27,27 @@ int main() {
         pas[i][i] = 1;
     }
 
+    for (int i = 0; i <= 20; ++i) {
+        pwr[i][0] = 1;
+        for (int j = 1; j <= 10; ++j) {
+            pwr[i][j] = pwr[i][j - 1] * i;
+        }
+    }
+
     int n;
     cin >> n;
 
     for (int i = 0; i < n; ++i) {
-        long r, s, w;
-        int x, y;
+        int r, s, x, y, w;
         cin >> r >> s >> x >> y >> w;
 
-        long pw = s - r + 1;
-        long pl = r - 1;
-
-        long aw[12];
-        long al[12];
-        aw[0] = 1;
-        al[0] = 1;
-        for (int j = 1; j <= y; ++j) {
-            aw[j] = aw[j - 1] * pw;
-            al[j] = al[j - 1] * pl;
-        }
+        int pw = s - r + 1;
+        int pl = r - 1;
 
         long pw_tot = 0;
         long p_tot = 0;
         for (int j = 0; j <= y; ++j) {
-            long p = pas[y][j] * aw[j] * al[y - j];
+            long p = pas[y][j] * pwr[pw][j] * pwr[pl][y - j];
             p_tot += p;
             if (j >= x) {
                 pw_tot += p;
