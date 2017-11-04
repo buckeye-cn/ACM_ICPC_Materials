af5ce30cae1ca57cf5fd3df814e716ca01dbb947
hcz <hczhcz@users.noreply.github.com>
2017-11-04 01:13:11 -0400

    Create happytrails_hcz.cpp

---
+++ solutions/kattis/ecna16/happytrails_hcz.cpp
@@ -0,0 +1,32 @@
+// https://open.kattis.com/problems/happytrails
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.precision(10);
+
+    int n;
+    cin >> n;
+
+    double h = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int a, l;
+        cin >> a >> l;
+
+        h += l * sin(a * acos(0) / 90);
+    }
+
+    printf("%.2f\n", h);
+
+    return 0;
+}
