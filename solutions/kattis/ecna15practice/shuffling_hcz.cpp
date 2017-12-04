40c90be6fe653ce53ad1c6f90fe909b2a6b888f3
hcz <hczhcz@users.noreply.github.com>
2017-12-04 00:31:25 -0500

    Create shuffling_hcz.cpp

---
+++ solutions/kattis/ecna15practice/shuffling_hcz.cpp
@@ -0,0 +1,62 @@
+// https://open.kattis.com/problems/shuffling
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(15);
+
+    int n;
+    string io;
+    cin >> n >> io;
+
+    int list[n];
+    for (int i = 0; i < n; ++i) {
+        list[i] = i;
+    }
+
+    for (int count = 1; true; ++count) {
+        int list2[n];
+
+        if (io == "out") {
+            for (int i = 0; i < n; ++i) {
+                int i2 = i - (n + 1) / 2;
+                if (i2 < 0) {
+                    list2[2 * i] = list[i];
+                } else {
+                    list2[2 * i2 + 1] = list[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < n; ++i) {
+                int i2 = i - n / 2;
+                if (i2 < 0) {
+                    list2[2 * i + 1] = list[i];
+                } else {
+                    list2[2 * i2] = list[i];
+                }
+            }
+        }
+
+        bool ok = true;
+        for (int i = 0; i < n; ++i) {
+            list[i] = list2[i];
+            ok &= list[i] == i;
+        }
+
+        if (ok) {
+            cout << count << endl;
+
+            return 0;
+        }
+    }
+}
