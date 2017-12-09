ef8340b67727d26f2df4534189ea411453790438
hcz <hczhcz@users.noreply.github.com>
2017-12-09 01:16:05 -0500

    Create carrots_hcz.cpp

---
+++ solutions/kattis/kth/carrots_hcz.cpp
@@ -0,0 +1,24 @@
+// https://open.kattis.com/problems/carrots
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int n, p;
+    cin >> n >> p;
+
+    cout << p << endl;
+
+    return 0;
+}
