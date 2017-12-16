2bfac542ed992a54e582aee45935ab4d2436b013
hcz <hczhcz@users.noreply.github.com>
2017-12-16 01:08:48 -0500

    Create twostones_hcz.cpp

---
+++ solutions/kattis/asiahk16preliminary/twostones_hcz.cpp
@@ -0,0 +1,24 @@
+// https://open.kattis.com/problems/twostones
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int n;
+    cin >> n;
+
+    cout << (n % 2 ? "Alice" : "Bob") << endl;
+
+    return 0;
+}
