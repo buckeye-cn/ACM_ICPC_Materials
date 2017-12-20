b01af138d2ad0e502f6026041e48a8363258fc13
Laurence <liuxy6@gmail.com>
2017-12-19 23:29:33 -0500

    Update units

--- solutions/kattis/naq14/units_laurence.cpp
+++ solutions/kattis/naq14/units_laurence.cpp
@@ -1,29 +1,28 @@
 #include <cmath>
 #include <iostream>
-#include <unordered_map>
+#include <map>
 
 using namespace std;
-
-inline bool gt(double a, double b) noexcept {
-    return (a - b) > 0.001;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    map<string, unsigned> unitsr = {};
     while (true) {
+        unitsr.clear();
+
         unsigned n;
         cin >> n;
         if (n == 0) break;
 
         string units[10] = {};
-        unordered_map<string, unsigned> unitsr = {};
         double graph[10][10] = {};
 
         for (unsigned i = 0; i < n; i++) {
             cin >> units[i];
             unitsr[units[i]] = i;
+            graph[i][i] = 1.0;
         }
 
         for (unsigned i = 0; i < (n-1); i++) {
@@ -40,7 +39,7 @@ int main() {
         for (unsigned k = 0; k < n; k++) {
             for (unsigned i = 0; i < n; i++) {
                 for (unsigned j = 0; j < n; j++) {
-                    if (graph[i][k] != 0.0 && graph[k][j] != 0.0)
+                    if (graph[i][j] == 0.0 && graph[i][k] != 0.0 && graph[k][j] != 0.0)
                         graph[i][j] = graph[i][k] * graph[k][j];
                     if (graph[i][j] > _max) {
                         _max = graph[i][j];
@@ -54,9 +53,9 @@ int main() {
         cout << "1" << units[max_i];
         for (unsigned _i = 0; _i < (n-1); _i++) {
             unsigned min_j = 0;
-            double _min = 2e32;
+            double _min = 1e32;
             for (unsigned j = 0; j < n; j++) {
-                if (gt(graph[cur_i][j], 1.0) && (graph[cur_i][j] < _min)) {
+                if (1.0 < graph[cur_i][j] && graph[cur_i][j] < _min) {
                     min_j = j;
                     _min = graph[cur_i][j];
                 }
