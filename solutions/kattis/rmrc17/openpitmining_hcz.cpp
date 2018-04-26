88cccc59259657d16c9855883e33c5fc7d91401d
hcz <hczhcz@users.noreply.github.com>
2018-04-26 00:35:10 -0400

    Create openpitmining_hcz.cpp

---
+++ solutions/kattis/rmrc17/openpitmining_hcz.cpp
@@ -0,0 +1,110 @@
+// https://open.kattis.com/problems/openpitmining
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+template <long N>
+struct EdmondsKarp {
+    struct Edge {
+        long from, to, cap;
+    };
+
+    vector<Edge> edges;
+    vector<long> outs[N];
+
+    void add(long from, long to, long cap){
+        if (cap > 0) {
+            edges.push_back(Edge {from, to, cap});
+            outs[from].push_back(edges.size() - 1);
+            edges.push_back(Edge {to, from, 0});
+            outs[to].push_back(edges.size() - 1);
+        }
+    }
+
+    long amount[N];
+    long route[N];
+
+    long solve(long from, long to){
+        long flow = 0;
+
+        while (true) {
+            queue<long> q;
+
+            memset(amount, 0, sizeof(amount));
+
+            q.push(from);
+
+            amount[from] = 1l << 60;
+            while (!q.empty() && !amount[to]){
+                long i = q.front();
+                q.pop();
+
+                for (long j = 0; j < outs[i].size(); ++j){
+                    Edge &e = edges[outs[i][j]];
+
+                    if (!amount[e.to] && e.cap){
+                        amount[e.to] = min(amount[i], e.cap);
+                        route[e.to] = outs[i][j];
+
+                        q.push(e.to);
+                    }
+                }
+            }
+            if (!amount[to]) break;
+
+            for (long i = to; i != from; i = edges[route[i]].from){
+                edges[route[i]].cap -= amount[to];
+                edges[route[i] ^ 1].cap += amount[to];
+            }
+
+            flow += amount[to];
+        }
+
+        return flow;
+    }
+};
+
+EdmondsKarp<240> ek;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int n;
+    cin >> n;
+
+    int full = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int value, cost, m;
+        cin >> value >> cost >> m;
+
+        if (value > cost) {
+            full += value - cost;
+            ek.add(238, i, value - cost);
+        } else {
+            ek.add(i, 239, cost - value);
+        }
+
+        for (int j = 0; j < m; ++j) {
+            int from;
+            cin >> from;
+
+            ek.add(from - 1, i, 1 << 30);
+        }
+    }
+
+    cout << full - ek.solve(238, 239) << endl;
+
+    return 0;
+}
