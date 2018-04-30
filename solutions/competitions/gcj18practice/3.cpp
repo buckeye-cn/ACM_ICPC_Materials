c7c178f0afcd973c3ee41d8007548804340aebec
hcz <hczhcz@users.noreply.github.com>
2018-04-30 00:55:53 -0400

    Create 3.cpp

---
+++ solutions/competitions/gcj18practice/3.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int t;
+    cin >> t;
+
+    for (int n_case = 1; n_case <= t; ++n_case) {
+        // cout << "Case #" << n_case << ": ";
+
+        double d;
+        int n;
+        cin >> d >> n;
+
+        double best = 1e13;
+
+        for (int i = 0; i < n; ++i) {
+            double k, s;
+            cin >> k >> s;
+
+            double sx = s * d / (d - k);
+
+            if (best > sx){
+                best = sx;
+            }
+        }
+
+        printf("Case #%d: %.10f\n", n_case, best);
+    }
+
+    return 0;
+}
