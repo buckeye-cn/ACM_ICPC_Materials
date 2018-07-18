3c615ac81b171baa4094711b71a191062ae6b39e
hcz <h.cz@qq.com>
2018-07-18 03:17:54 -0400

    fix for kattis rejudging

--- solutions/kattis/ctu10/hack_hcz.cpp
+++ solutions/kattis/ctu10/hack_hcz.cpp
@@ -10,22 +10,24 @@
 
 using namespace std;
 
-int trans[7][1000000] = {
-    {},
-    {},
-    {
-        1,  2,  3,  4,  5,  6,  7,  8,  9,  19,
-        0,  21, 11, 12, 13, 14, 15, 16, 17, 18,
-        10, 22, 23, 24, 25, 26, 27, 28, 29, 39,
-        20, 41, 31, 32, 33, 34, 35, 36, 37, 38,
-        30, 42, 43, 44, 45, 46, 47, 48, 49, 59,
-        40, 61, 51, 52, 53, 54, 55, 56, 57, 58,
-        50, 62, 63, 64, 65, 66, 67, 68, 69, 79,
-        60, 81, 71, 72, 73, 74, 75, 76, 77, 78,
-        70, 82, 83, 84, 85, 86, 87, 88, 89, 99,
-        80, 90, 91, 92, 93, 94, 95, 96, 97, 98,
-    }
+int trans2[100] = {
+    1,  2,  3,  4,  5,  6,  7,  8,  9,  19,
+    0,  21, 11, 12, 13, 14, 15, 16, 17, 18,
+    10, 22, 23, 24, 25, 26, 27, 28, 29, 39,
+    20, 41, 31, 32, 33, 34, 35, 36, 37, 38,
+    30, 42, 43, 44, 45, 46, 47, 48, 49, 59,
+    40, 61, 51, 52, 53, 54, 55, 56, 57, 58,
+    50, 62, 63, 64, 65, 66, 67, 68, 69, 79,
+    60, 81, 71, 72, 73, 74, 75, 76, 77, 78,
+    70, 82, 83, 84, 85, 86, 87, 88, 89, 99,
+    80, 90, 91, 92, 93, 94, 95, 96, 97, 98,
 };
+int trans3[1000];
+int trans4[10000];
+int trans5[100000];
+int trans6[1000000];
+
+int *trans[7] = {nullptr, nullptr, trans2, trans3, trans4, trans5, trans6};
 
 int p10(int x) {
     int result = 1;
@@ -39,7 +41,7 @@ int p10(int x) {
 
 void write_num(int x, int size) {
     for (int i = 0; i < size; ++i) {
-        cout << char('0' + x / p10(size - i - 1) % 10);
+        putchar('0' + x / p10(size - i - 1) % 10);
     }
 }
 
@@ -84,32 +86,32 @@ int main() {
 
         if (s.size() == 1) {
             switch (s[0]) {
-                case '0': cout << "9"  << endl << "1 2 3 4 5 6 7 8 9" << endl; break;
-                case '1': cout << "10" << endl << "0 1 2 3 4 5 6 7 8 9" << endl; break;
-                case '2': cout << "11" << endl << "1 0 1 2 3 4 5 6 7 8 9" << endl; break;
-                case '3': cout << "12" << endl << "2 1 0 1 2 3 4 5 6 7 8 9" << endl; break;
-                case '4': cout << "13" << endl << "3 2 1 0 1 2 3 4 5 6 7 8 9" << endl; break;
-                case '5': cout << "13" << endl << "6 7 8 9 8 7 6 5 4 3 2 1 0" << endl; break;
-                case '6': cout << "12" << endl << "7 8 9 8 7 6 5 4 3 2 1 0" << endl; break;
-                case '7': cout << "11" << endl << "8 9 8 7 6 5 4 3 2 1 0" << endl; break;
-                case '8': cout << "10" << endl << "9 8 7 6 5 4 3 2 1 0" << endl; break;
-                case '9': cout << "9"  << endl << "8 7 6 5 4 3 2 1 0" << endl; break;
+                case '0': printf("9\n1 2 3 4 5 6 7 8 9\n"); break;
+                case '1': printf("10\n0 1 2 3 4 5 6 7 8 9\n"); break;
+                case '2': printf("11\n1 0 1 2 3 4 5 6 7 8 9\n"); break;
+                case '3': printf("12\n2 1 0 1 2 3 4 5 6 7 8 9\n"); break;
+                case '4': printf("13\n3 2 1 0 1 2 3 4 5 6 7 8 9\n"); break;
+                case '5': printf("13\n6 7 8 9 8 7 6 5 4 3 2 1 0\n"); break;
+                case '6': printf("12\n7 8 9 8 7 6 5 4 3 2 1 0\n"); break;
+                case '7': printf("11\n8 9 8 7 6 5 4 3 2 1 0\n"); break;
+                case '8': printf("10\n9 8 7 6 5 4 3 2 1 0\n"); break;
+                case '9': printf("9\n8 7 6 5 4 3 2 1 0\n"); break;
             }
         } else {
             int tot = p10(s.size()) - 1;
             int pos = stoi(s, 0, 10);
 
             pos = trans[s.size()][pos];
-            cout << tot << endl;
+            printf("%d\n", tot);
             write_num(pos, s.size());
 
             for (int i = 1; i < tot; ++i) {
                 pos = trans[s.size()][pos];
-                cout << ' ';
+                putchar(' ');
                 write_num(pos, s.size());
             }
 
-            cout << endl;
+            putchar('\n');
         }
     }
 }
