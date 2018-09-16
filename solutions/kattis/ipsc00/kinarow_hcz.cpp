4d9ab3726409497f3688147f2fb80897e18e33db
hcz <hczhcz@users.noreply.github.com>
2018-09-15 22:16:51 -0400

    Create kinarow_hcz.cpp

---
+++ solutions/kattis/ipsc00/kinarow_hcz.cpp
@@ -0,0 +1,98 @@
+// https://open.kattis.com/problems/kinarow
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+char data[200][200];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int l;
+    cin >> l;
+
+    int xwin = 0;
+    int owin = 0;
+
+    for (; l > 0; --l) {
+        int m, n, r;
+        cin >> m >> n >> r;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                cin >> data[i][j];
+            }
+        }
+
+        bool xx = false;
+        bool oo = false;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m - r + 1; ++j) {
+                char now = data[i][j];
+
+                for (int k = 1; k < r; ++k) {
+                    if (data[i][j + k] != now) now = '.';
+                }
+
+                if (now == 'x') xx = true;
+                if (now == 'o') oo = true;
+            }
+        }
+
+        for (int i = 0; i < n - r + 1; ++i) {
+            for (int j = 0; j < m; ++j) {
+                char now = data[i][j];
+
+                for (int k = 1; k < r; ++k) {
+                    if (data[i + k][j] != now) now = '.';
+                }
+
+                if (now == 'x') xx = true;
+                if (now == 'o') oo = true;
+            }
+        }
+
+        for (int i = 0; i < n - r + 1; ++i) {
+            for (int j = r - 1; j < m; ++j) {
+                char now = data[i][j];
+
+                for (int k = 1; k < r; ++k) {
+                    if (data[i + k][j - k] != now) now = '.';
+                }
+
+                if (now == 'x') xx = true;
+                if (now == 'o') oo = true;
+            }
+        }
+
+        for (int i = 0; i < n - r + 1; ++i) {
+            for (int j = 0; j < m - r + 1; ++j) {
+                char now = data[i][j];
+
+                for (int k = 1; k < r; ++k) {
+                    if (data[i + k][j + k] != now) now = '.';
+                }
+
+                if (now == 'x') xx = true;
+                if (now == 'o') oo = true;
+            }
+        }
+
+        xwin += xx;
+        owin += oo;
+    }
+
+    cout << xwin << ':' << owin << endl;
+
+    return 0;
+}
