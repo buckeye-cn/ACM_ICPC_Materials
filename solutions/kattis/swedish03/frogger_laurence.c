478748d41015272902328c479662450910acf26d
Laurence <liuxy6@gmail.com>
2018-09-21 23:55:32 -0400

    Fix memory access out of bounds

--- solutions/kattis/swedish03/frogger_laurence.c
+++ solutions/kattis/swedish03/frogger_laurence.c
@@ -3,53 +3,57 @@
 #include <string.h>
 
 #define MAX(a,b) (((a)>(b))?(a):(b))
+#define SWAP(x,y) { typeof(x) _x = x; typeof(y) _y = y; x = _y; y = _x; }
 
-int X, N, M;
-int from, to;
-int depth;
-uint8_t board[100007][23][53];
+typedef uint8_t Board[25][55];
+Board board[2];
 
 int main() {
     int _N;
     scanf("%d", &_N);
 
+    Board *prev = &board[0], *curr = &board[1];
+
     for (int _i = 0; _i < _N; _i++) {
-        memset(board, 0, (MAX(depth, X)+1)*23*53);
+        memset(board, 0, sizeof(board));
+
+        int X, N, M;
+        int to;
 
         scanf("%d %d %d", &X, &N, &M);
         getchar();
-        for (int i = 1; i <= M; i++)
-            if (getchar() == 'G')
-                to = i;
-        getchar();
+
+        N += 2;
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++)
-                if (getchar() == 'X')
-                    board[0][i][j] = 3;
+                switch (getchar()) {
+                    case 'G': to = j; break;
+                    case 'F': (*curr)[N][j] = 1; break;
+                    case 'X': (*curr)[i][j] = 3; break;
+                }
             getchar();
         }
-        for (int i = 1; i <= M; i++)
-            if (getchar() == 'F')
-                from = i;
-        board[0][N+1][from] = 1;
 
+        int depth;
         for (depth = 1; depth <= X; depth++) {
-            for (int i = 0; i <= N+1; i++) {
+            SWAP(prev, curr);
+            memset(curr, 0, sizeof(Board));
+            for (int i = 1; i <= N; i++) {
                 for (int j = 1; j <= M; j++) {
-                    uint8_t pc = board[depth-1][i][j];
+                    uint8_t pc = (*prev)[i][j];
                     if (pc == 1) {
-                        board[depth][i][j]   |= 1;
-                        //board[depth][i+1][j] |= 1;
-                        board[depth][i-1][j] |= 1;
-                        board[depth][i][j+1] |= 1;
-                        board[depth][i][j-1] |= 1;
+                        (*curr)[i][j]   |= 1;
+                        //(*curr)[i+1][j] |= 1;
+                        (*curr)[i-1][j] |= 1;
+                        (*curr)[i][j+1] |= 1;
+                        (*curr)[i][j-1] |= 1;
                     } else if (pc == 3) {
-                        if (~(i ^ N) & 1) board[depth][i][(j+1 > M) ? 1 : j+1] = 3;
-                        else              board[depth][i][(j-1 < 1) ? M : j-1] = 3;
+                        if ((i ^ N) & 1) (*curr)[i][(j+1 > M) ? 1 : j+1] = 3;
+                        else             (*curr)[i][(j-1 < 1) ? M : j-1] = 3;
                     }
                 }
             }
-            if (board[depth][0][to])
+            if ((*curr)[1][to])
                 break;
         }
         if (depth > X)
