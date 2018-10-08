96811c5eb3aa81226c294087605a037e46084608
hcz <hczhcz@users.noreply.github.com>
2018-10-08 02:56:36 -0400

    Create superdoku_hcz.cpp

---
+++ solutions/kattis/naq18/superdoku_hcz.cpp
@@ -0,0 +1,149 @@
+// https://open.kattis.com/problems/superdoku
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+template <long N>
+struct EdmondsKarp {
+    struct Edge {
+        long from, to, cap;
+    };
+
+    vector<Edge> edges;
+    vector<long> outs[N];
+
+    void add(long from, long to, long cap){
+        if (cap > 0) {
+            edges.push_back(Edge {from, to, cap});
+            outs[from].push_back(edges.size() - 1);
+            edges.push_back(Edge {to, from, 0});
+            outs[to].push_back(edges.size() - 1);
+        }
+    }
+
+    long amount[N];
+    long route[N];
+
+    long solve(long from, long to){
+        long flow = 0;
+
+        while (true) {
+            queue<long> q;
+
+            memset(amount, 0, sizeof(amount));
+
+            q.push(from);
+
+            amount[from] = 1l << 60;
+            while (!q.empty() && !amount[to]){
+                long i = q.front();
+                q.pop();
+
+                for (long j = 0; j < outs[i].size(); ++j){
+                    Edge &e = edges[outs[i][j]];
+
+                    if (!amount[e.to] && e.cap){
+                        amount[e.to] = min(amount[i], e.cap);
+                        route[e.to] = outs[i][j];
+
+                        q.push(e.to);
+                    }
+                }
+            }
+            if (!amount[to]) break;
+
+            for (long i = to; i != from; i = edges[route[i]].from){
+                edges[route[i]].cap -= amount[to];
+                edges[route[i] ^ 1].cap += amount[to];
+            }
+
+            flow += amount[to];
+        }
+
+        return flow;
+    }
+};
+
+EdmondsKarp<202> ek[100];
+
+int n, k;
+int data[100][100];
+__int128 mask_i[100];
+__int128 mask_j[100];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    cin >> n >> k;
+
+    for (int i = 0; i < k; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> data[i][j];
+            data[i][j] -= 1;
+
+            __int128 mask = mask_i[i] | mask_j[j];
+            __int128 m = __int128(1) << data[i][j];
+
+            if (!(mask & m)) {
+                mask_i[i] ^= m;
+                mask_j[j] ^= m;
+            } else {
+                cout << "no" << endl;
+
+                return 0;
+            }
+        }
+    }
+
+    for (int i = k; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            ek[i].add(200, j, 1);
+            ek[i].add(100 + j, 201, 1);
+        }
+
+        for (int j = 0; j < n; ++j) {
+            for (int k = 0; k < n; ++k) {
+                if ((mask_j[j] & (__int128(1) << k)) == 0) {
+                    ek[i].add(j, 100 + k, 1);
+                }
+            }
+        }
+
+        ek[i].solve(200, 201);
+
+        for (auto &edge: ek[i].edges) {
+            if (!edge.cap && edge.from < 100 && edge.to < 200) {
+                int j = edge.from;
+                int k = edge.to - 100;
+
+                data[i][j] = k;
+                mask_j[j] ^= __int128(1) << k;
+            }
+        }
+    }
+
+    cout << "yes" << endl;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (j) {
+                cout << ' ';
+            }
+            cout << data[i][j] + 1;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
