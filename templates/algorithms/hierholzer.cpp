b14d91d360469a04e7df8c89f83fcb9dd1cc5dfd
hcz <h.cz@qq.com>
2018-11-08 22:29:48 -0500

    add hierholzer template

---
+++ templates/algorithms/hierholzer.cpp
@@ -0,0 +1,41 @@
+// Eulerian Circuit
+
+template <long N>
+struct Hierholzer {
+    struct Edge {
+        long to;
+        bool chosen;
+    };
+
+    vector<Edge> edges;
+    vector<long> outs[N];
+
+    void add(long from, long to) {
+        edges.push_back(Edge {to, false});
+        outs[from].push_back(edges.size() - 1);
+        edges.push_back(Edge {from, false});
+        outs[to].push_back(edges.size() - 1);
+    }
+
+    long step[N];
+
+    void dfs(vector<long> &path, long from) {
+        for (; step[from] < outs[from].size(); ++step[from]) {
+            Edge &e = edges[outs[from][step[from]]];
+
+            if (!e.chosen) {
+                e.chosen = true;
+                edges[outs[from][step[from]] ^ 1].chosen = true;
+                dfs(path, e.to);
+            }
+        }
+
+        path.push_back(from);
+    }
+
+    void solve(vector<long> &path, long from) {
+        memset(step, 0, sizeof(step));
+
+        dfs(path, from);
+    }
+};
