2626cd4708a757e9205aca7640716d8896b4ded0
hcz <hczhcz@users.noreply.github.com>
2018-11-16 02:57:58 -0500

    Create toursdesalesforce_hcz.cpp

---
+++ solutions/kattis/ecna18/toursdesalesforce_hcz.cpp
@@ -0,0 +1,246 @@
+// https://open.kattis.com/problems/toursdesalesforce
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#define sqr(x) ((x) * (x))
+
+using namespace std;
+
+template <long N>
+struct HeldKarp {
+    struct Edge {
+        long to, len;
+    };
+
+    vector<Edge> edges;
+    vector<long> outs[N];
+
+    void add(long from, long to, long len) {
+        if (len >= 0) {
+            edges.push_back({to, len});
+            outs[from].push_back(edges.size() - 1);
+        }
+    }
+
+    long dist[N][N];
+    long best[1l << (N - 1)][N];
+
+    long solve(long n) {
+        for (long i = 0; i < n; ++i) {
+            for (long j = 0; j < n; ++j) {
+                dist[i][j] = 1l << 60;
+            }
+
+            dist[i][i] = 0;
+
+            for (long j = 0; j < outs[i].size(); ++j) {
+                Edge &e = edges[outs[i][j]];
+
+                dist[i][e.to] = min(dist[i][e.to], e.len);
+            }
+        }
+
+        for (long i = 0; i < n; ++i) {
+            for (long j = 0; j < n; ++j) {
+                for (long k = 0; k < n; ++k) {
+                    dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
+                }
+            }
+        }
+
+        for (long i = 0; i < (1l << (n - 1)); ++i) {
+            for (long j = 0; j < n; ++j) {
+                best[i][j] = 1l << 60;
+            }
+
+            for (long j = 0; j < n - 1; ++j) {
+                if (i == 1l << j) {
+                    best[i][j] = dist[n - 1][j];
+                } else if (i & (1l << j)) {
+                    for (long k = 0; k < n - 1; ++k) {
+                        if ((i ^ (1l << j)) & (1l << k)) {
+                            best[i][j] = min(best[i][j], dist[j][k] + best[i ^ (1l << j)][k]);
+                        }
+                    }
+                }
+            }
+        }
+
+        long result = 1l << 60;
+        for (long i = 0; i < n - 1; ++i) {
+            result = min(result, dist[i][n - 1] + best[(1l << (n - 1)) - 1][i]);
+        }
+
+        return result;
+    }
+};
+
+template <long N>
+struct EdmondsKarpMinCost {
+    struct Edge {
+        long from, to, cap, len;
+    };
+
+    vector<Edge> edges;
+    vector<long> outs[N];
+
+    void add(long from, long to, long cap, long len) {
+        if (cap > 0) {
+            edges.push_back(Edge {from, to, cap, len});
+            outs[from].push_back(edges.size() - 1);
+            edges.push_back(Edge {to, from, 0, -len});
+            outs[to].push_back(edges.size() - 1);
+        }
+    }
+
+    long amount[N];
+    long dist[N];
+    long route[N];
+    long visiting[N * N];
+
+    pair<long, long> solve(long from, long to) {
+        long flow = 0;
+        long cost = 0;
+
+        while (true) {
+            memset(amount, 0, sizeof(amount));
+
+            for (int i = 0; i < N; ++i) {
+                dist[i] = 1l << 60;
+            }
+
+            long head = 0;
+            long tail = 0;
+
+            amount[from] = 1l << 60;
+            dist[from] = 0;
+            visiting[tail++] = from;
+
+            while (head < tail) {
+                long i = visiting[head++];
+
+                for (long j = 0; j < outs[i].size(); ++j) {
+                    Edge &e = edges[outs[i][j]];
+
+                    if (e.cap && dist[e.to] > dist[i] + e.len) {
+                        amount[e.to] = min(amount[i], e.cap);
+                        dist[e.to] = dist[i] + e.len;
+                        route[e.to] = outs[i][j];
+
+                        visiting[tail++] = e.to;
+                    }
+                }
+            }
+
+            if (!amount[to]) break;
+
+            for (long i = to; i != from; i = edges[route[i]].from) {
+                edges[route[i]].cap -= amount[to];
+                edges[route[i] ^ 1].cap += amount[to];
+            }
+
+            flow += amount[to];
+            cost += amount[to] * dist[to];
+        }
+
+        return {flow, cost};
+    }
+};
+
+HeldKarp<16> hk;
+EdmondsKarpMinCost<52> ek;
+
+int d;
+int n[50];
+long x[50][8];
+long y[50][8];
+long cost[50];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    cin >> d;
+
+    for (int i = 0; i < d; ++i) {
+        cin >> n[i];
+
+        for (int j = 0; j < n[i]; ++j) {
+            cin >> x[i][j] >> y[i][j];
+        }
+    }
+
+    for (int i = 0; i < d; ++i) {
+        hk.edges.clear();
+        for (int j = 0; j < 16; ++j) {
+            hk.outs[j].clear();
+        }
+
+        for (int j = 0; j < n[i]; ++j) {
+            for (int k = 0; k < n[i]; ++k) {
+                hk.add(j, k, round(sqrt(sqr(x[i][j] - x[i][k]) + sqr(y[i][j] - y[i][k])) * 1e6));
+            }
+        }
+
+        cost[i] = hk.solve(n[i]);
+
+        if (i < d / 2) {
+            ek.add(50, i, 1, 0);
+        } else {
+            ek.add(i, 51, 1, 0);
+        }
+
+        // cerr << i << endl;
+    }
+
+    for (int i = 0; i < d / 2; ++i) {
+        for (int j = d / 2; j < d; ++j) {
+            hk.edges.clear();
+            for (int k = 0; k < 16; ++k) {
+                hk.outs[k].clear();
+            }
+
+            for (int k = 0; k < n[i]; ++k) {
+                for (int l = 0; l < n[i]; ++l) {
+                    hk.add(k, l, round(sqrt(sqr(x[i][k] - x[i][l]) + sqr(y[i][k] - y[i][l])) * 1e6));
+                }
+
+                for (int l = 0; l < n[j]; ++l) {
+                    hk.add(k, n[i] + l, round(sqrt(sqr(x[i][k] - x[j][l]) + sqr(y[i][k] - y[j][l])) * 1e6));
+                }
+            }
+
+            for (int k = 0; k < n[j]; ++k) {
+                for (int l = 0; l < n[i]; ++l) {
+                    hk.add(n[i] + k, l, round(sqrt(sqr(x[j][k] - x[i][l]) + sqr(y[j][k] - y[i][l])) * 1e6));
+                }
+
+                for (int l = 0; l < n[j]; ++l) {
+                    hk.add(n[i] + k, n[i] + l, round(sqrt(sqr(x[j][k] - x[j][l]) + sqr(y[j][k] - y[j][l])) * 1e6));
+                }
+            }
+
+            ek.add(i, j, 1, hk.solve(n[i] + n[j]) - cost[i] - cost[j]);
+
+            // cerr << i << ' ' << j << endl;
+        }
+    }
+
+    long tot = 0;
+
+    for (int i = 0; i < d; ++i) {
+        tot += cost[i];
+    }
+
+    cout << tot * 1e-6 << ' ' << (tot + ek.solve(50, 51).second) * 1e-6 << endl;
+
+    return 0;
+}
