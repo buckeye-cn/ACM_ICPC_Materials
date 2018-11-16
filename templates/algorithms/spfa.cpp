ec176744560f2e53deb741cc432cf805995c387c
hcz <h.cz@qq.com>
2018-11-16 03:49:27 -0500

    add spfa template and example

---
+++ templates/algorithms/spfa.cpp
@@ -0,0 +1,57 @@
+// Shortest Path
+
+template <long N>
+struct SPFA {
+    struct Edge {
+        long to, len;
+    };
+
+    vector<Edge> edges;
+    vector<long> outs[N];
+
+    void add(long from, long to, long len) {
+        edges.push_back({to, len});
+        outs[from].push_back(edges.size() - 1);
+    }
+
+    long dist[N];
+    long route[N];
+    long visiting[N];
+    bool active[N];
+
+    long solve(long from, long to) {
+        memset(active, 0, sizeof(active));
+
+        for (int i = 0; i < N; ++i) {
+            dist[i] = 1l << 60;
+        }
+
+        long head = 0;
+        long tail = 0;
+
+        dist[from] = 0;
+        visiting[(tail++) % N] = from;
+        active[from] = true;
+
+        while (head < tail) {
+            long i = visiting[(head++) % N];
+            active[i] = false;
+
+            for (long j = 0; j < outs[i].size(); ++j) {
+                Edge &e = edges[outs[i][j]];
+
+                if (dist[e.to] > dist[i] + e.len) {
+                    dist[e.to] = dist[i] + e.len;
+                    route[e.to] = outs[i][j];
+
+                    if (!active[e.to]) {
+                        visiting[(tail++) % N] = e.to;
+                        active[e.to] = true;
+                    }
+                }
+            }
+        }
+
+        return dist[to];
+    }
+};
