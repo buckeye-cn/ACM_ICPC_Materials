95cf660b1b023fbf03fddb2d8501216e0bc60ca9
hcz <hczhcz@users.noreply.github.com>
2021-03-12 01:42:38 +0800

    Update usingdigits_hcz.cpp

--- solutions/kattis/naq20/usingdigits_hcz.cpp
+++ solutions/kattis/naq20/usingdigits_hcz.cpp
@@ -10,9 +10,7 @@
 
 using namespace std;
 
-char v[101][101];
-char h[51];
-int best[101][101][51];
+int16_t best[101][101][51];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -23,25 +21,26 @@ int main() {
     string s;
     cin >> x >> y >> s;
 
-    h[0] = -1;
+    char h[s.size() + 1];
 
-    for (int i = 1; i <= s.size(); ++i) {
-        h[i] = s[i - 1] - '0';
+    for (int k = 1; k <= s.size(); ++k) {
+        h[k] = s[k - 1] - '0';
     }
 
-    for (int i = y; i >= 1; --i) {
-        cin >> s;
-
-        for (int j = 1; j <= x; ++j) {
-            v[i][j] = s[j - 1] - '0';
-        }
-    }
-
-    best[0][1][0] = -100000;
+    char v[y + 1][x + 1];
+    cin.read(&v[0][0], (x + 1) * y);
 
     for (int i = 1; i <= y; ++i) {
         for (int j = 1; j <= x; ++j) {
-            for (int k = 0; h[k]; ++k) {
+            v[y - i][j] -= '0';
+        }
+    }
+
+    best[0][1][0] = -10000;
+
+    for (int i = 1; i <= y; ++i) {
+        for (int j = 1; j <= x; ++j) {
+            for (int k = 0; k <= s.size(); ++k) {
                 best[i][j][k] = min(best[i - 1][j][k], best[i][j - 1][k]);
 
                 if (k && i - h[k] >= 2) {
@@ -52,18 +51,18 @@ int main() {
                     best[i][j][k] = min(best[i][j][k], best[i][j - h[k] - 1][k - 1]);
                 }
 
-                best[i][j][k] += v[i][j];
+                best[i][j][k] += v[y - i][j];
             }
         }
     }
 
-    int result = 0;
+    int16_t result = 0;
 
-    for (int k = 0; h[k]; ++k) {
+    for (int k = 0; k <= s.size(); ++k) {
         result = min(result, best[y][x][k]);
     }
 
-    cout << result + 100000 << endl;
+    cout << result + 10000 << endl;
 
     return 0;
 }
