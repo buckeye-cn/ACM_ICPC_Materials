0cc85021e976bad9069a3eb50381ace8d0f61a88
hcz <hczhcz@users.noreply.github.com>
2022-03-04 20:26:47 +0800

    Create statues_hcz.cpp

---
+++ solutions/kattis/ecna21/statues_hcz.cpp
@@ -0,0 +1,79 @@
+// https://open.kattis.com/problems/statues
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+int nums[2500];
+int grid[50][50];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int n, m;
+    cin >> n >> m;
+
+    set<int> s;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> grid[i][j];
+
+            if (grid[i][j] > 0) {
+                s.insert(grid[i][j]);
+            }
+        }
+    }
+
+    int tot = 0;
+
+    for (int i: s) {
+        nums[tot++] = i;
+    }
+    nums[tot] = 2147483647;
+
+    int best = n * m;
+
+    for (int mode = 0; mode < 4; ++mode) {
+        int curr = 0;
+        int pos = mode < 2 ? 0 : tot;
+
+        for (int t = 0; t < n + m - 1; ++t) {
+            int tail = pos;
+
+            for (int i = max(0, t - m + 1); i < min(n, t + 1); ++i) {
+                int j = mode % 2 == 0 ? t - i : m - 1 - t + i;
+
+                if (grid[i][j] > 0) {
+                    tail += mode < 2 ? 1 : -1;
+                }
+            }
+
+            for (int i = max(0, t - m + 1); i < min(n, t + 1); ++i) {
+                int j = mode % 2 == 0 ? t - i : m - 1 - t + i;
+
+                if (grid[i][j] > 0 && (grid[i][j] >= nums[pos]) == (grid[i][j] >= nums[tail])) {
+                    curr += 1;
+                }
+            }
+
+            pos = tail;
+            // cerr << mode << '\t' << curr << endl;
+        }
+
+        best = min(best, curr);
+    }
+
+    cout << best << endl;
+
+    return 0;
+}
