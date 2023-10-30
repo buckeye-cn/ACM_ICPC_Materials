7e7308a6e90fb3b7b66ff68fa3272750e416b234
hcz <hczhcz@users.noreply.github.com>
2023-10-31 00:15:43 +0800

    Create cornhusker_hcz.cpp

---
+++ solutions/kattis/ecna23/cornhusker_hcz.cpp
@@ -0,0 +1,35 @@
+// https://open.kattis.com/problems/cornhusker
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int v[5];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int s = 0;
+
+    for (int i = 0; i < 5; ++i) {
+        int a, l;
+        cin >> a >> l;
+
+        s += a * l;
+    }
+
+    int n, kwf;
+    cin >> n >> kwf;
+
+    cout << s / 5 * n / kwf << endl;
+
+    return 0;
+}
