d0d55754b87360780454f7dce35812fc3c6ecd44
hcz <hczhcz@users.noreply.github.com>
2023-10-31 20:28:49 +0800

    Create broadband_hcz.cpp

---
+++ solutions/kattis/ecna23/broadband_hcz.cpp
@@ -0,0 +1,65 @@
+// https://open.kattis.com/problems/broadband
+
+#include <cstdlib>
+#include <cstdint>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <set>
+#include <iostream>
+
+#define sqr(x) ((x) * (x))
+
+using namespace std;
+
+double cumsum1[2001];
+double cumsum2[2001];
+double dp[2001][101];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.precision(10);
+
+    int m, n, ap;
+    double s;
+    cin >> m >> n >> ap >> s;
+
+    multiset<double> xs;
+
+    for (int i = 0; i < m + n; ++i) {
+        double x;
+        cin >> x;
+
+        xs.insert(x);
+    }
+
+    int pos = 0;
+
+    for (double x: xs) {
+        cumsum1[pos + 1] = cumsum1[pos] + x;
+        cumsum2[pos + 1] = cumsum2[pos] + x * x;
+        pos += 1;
+    }
+
+    for (int i = 1; i <= m + n; ++i) {
+        for (int j = 0; j <= ap; ++j) {
+            dp[i][j] = 1e9;
+        }
+    }
+
+    for (int i = 0; i < m + n; ++i) {
+        for (int j = i + 1; j <= m + n; ++j) {
+            double v = cumsum2[j] - cumsum2[i] - sqr(cumsum1[j] - cumsum1[i]) / (j - i);
+
+            for (int k = 0; k < ap; ++k) {
+                dp[j][k + 1] = min(dp[j][k + 1], dp[i][k] + v);
+            }
+        }
+    }
+
+    cout << dp[m + n][ap] + sqr(s) * (m + n) / 4 << endl;
+
+    return 0;
+}
