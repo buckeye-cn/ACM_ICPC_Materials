96cb15d4fb032ef454f7f1162c197caaab5f33bd
hcz <hczhcz@users.noreply.github.com>
2024-10-10 20:52:22 +0800

    Update catch_hcz.cpp

--- solutions/kattis/icpc18/catch_hcz.cpp
+++ solutions/kattis/icpc18/catch_hcz.cpp
@@ -6,7 +6,6 @@
 #include <cmath>
 #include <cstring>
 #include <string>
-#include <set>
 #include <iostream>
 
 using namespace std;
@@ -14,6 +13,7 @@ using namespace std;
 int a[1000000];
 int b[1000000];
 double pp[1000000];
+pair<long, int> ts[2000000];
 
 double p_prev[2000000];
 long t_prev[2000000];
@@ -29,37 +29,35 @@ int main() {
     long k;
     cin >> m >> n >> k;
 
-    set<pair<long, int>> ts;
-
     for (int i = 0; i < m; ++i) {
         long s, t;
         cin  >> a[i] >> b[i] >> s >> t >> pp[i];
 
-        ts.insert({-s * 2, i});
-        ts.insert({-t * 2 - 1, i});
+        ts[2 * i].first = -s * 2;
+        ts[2 * i].second = i;
+        ts[2 * i + 1].first = -t * 2 - 1;
+        ts[2 * i + 1].second = i;
     }
+
+    sort(ts, ts + 2 * m);
 
     p_curr[1] = 1;
 
-    for (pair<long, int> p: ts) {
-        int i = p.second;
+    for (int ii = 0; ii < 2 * m; ++ii) {
+        int i = ts[ii].second;
 
-        if (p.first & 1) {
+        if (ts[ii].first & 1) {
             p_bus[i] = p_curr[b[i]];
-
-            // cerr << 't' << -(p.first / 2) << '\t' << i << ' ' << b[i] << ' ' << p_bus[i] << endl;
         } else {
-            if (t_prev[a[i]] != p.first / 2) {
+            if (t_prev[a[i]] != ts[ii].first / 2) {
                 p_prev[a[i]] = p_curr[a[i]];
-                t_prev[a[i]] = p.first / 2;
+                t_prev[a[i]] = ts[ii].first / 2;
             }
 
             p_curr[a[i]] = max(
                 p_curr[a[i]],
                 pp[i] * p_bus[i] + (1 - pp[i]) * p_prev[a[i]]
             );
-
-            // cerr << 's' << -(p.first / 2) << '\t' << i << ' ' << a[i] << ' ' << p_curr[a[i]] << endl;
         }
     }
 
