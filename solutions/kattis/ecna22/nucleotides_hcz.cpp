3c4d8526482edb6cab7a4b49bd88eae47cc36829
hcz <hczhcz@users.noreply.github.com>
2024-10-10 21:06:03 +0800

    Update nucleotides_hcz.cpp

--- solutions/kattis/ecna22/nucleotides_hcz.cpp
+++ solutions/kattis/ecna22/nucleotides_hcz.cpp
@@ -6,7 +6,6 @@
 #include <cmath>
 #include <cstring>
 #include <string>
-#include <set>
 #include <iostream>
 
 using namespace std;
@@ -36,17 +35,21 @@ int main() {
         int s, e;
         cin >> s >> e;
 
-        set<pair<int, char>> atgc;
+        int atgc[4];
 
-        atgc.insert({-4 * (aa[e] - aa[s - 1]) - 3, 'A'});
-        atgc.insert({-4 * (tt[e] - tt[s - 1]) - 2, 'T'});
-        atgc.insert({-4 * (gg[e] - gg[s - 1]) - 1, 'G'});
-        atgc.insert({-4 * (cc[e] - cc[s - 1])    , 'C'});
+        atgc[0] = 0 - 4 * (aa[e] - aa[s - 1]);
+        atgc[1] = 1 - 4 * (tt[e] - tt[s - 1]);
+        atgc[2] = 2 - 4 * (gg[e] - gg[s - 1]);
+        atgc[3] = 3 - 4 * (cc[e] - cc[s - 1]);
 
-        for (pair<int, char> p: atgc) {
-            cout << p.second;
-        }
-        cout << endl;
+        if (atgc[0] > atgc[1]) swap(atgc[0], atgc[1]);
+        if (atgc[1] > atgc[2]) swap(atgc[1], atgc[2]);
+        if (atgc[2] > atgc[3]) swap(atgc[2], atgc[3]);
+        if (atgc[0] > atgc[1]) swap(atgc[0], atgc[1]);
+        if (atgc[1] > atgc[2]) swap(atgc[1], atgc[2]);
+        if (atgc[0] > atgc[1]) swap(atgc[0], atgc[1]);
+
+        cout << "ATGC"[atgc[0] & 3] << "ATGC"[atgc[1] & 3] << "ATGC"[atgc[2] & 3] << "ATGC"[atgc[3] & 3] << endl;
     }
 
     return 0;
