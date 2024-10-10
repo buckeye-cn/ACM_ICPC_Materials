95719b75deb9f3b68cbf6ffa67bb6c2bb513a8ae
hcz <hczhcz@users.noreply.github.com>
2024-10-10 21:22:27 +0800

    Update pasijans_hcz.cpp

--- solutions/kattis/coci09/pasijans_hcz.cpp
+++ solutions/kattis/coci09/pasijans_hcz.cpp
@@ -81,14 +81,14 @@ struct Comp {
     }
 };
 
-set<int, Comp> compset;
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.precision(10);
 
     cin >> n;
+
+    set<int, Comp> compset;
 
     for (int i = 0; i < n; ++i) {
         cin >> tot[i];
